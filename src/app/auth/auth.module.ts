945b476ad705cd1fca418863bbd6e4dd71d82c42
Fabio <fabioescobarardila@hotmail.com>
2024-03-25 14:55:20 -0500

    Project structure completed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -2,10 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { AuthRoutingModule } from './auth-routing.module';
+import { RegisterPageComponent } from './pages/register-page/register-page.component';
+import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    RegisterPageComponent,
+    AuthLayoutComponent
+  ],
   imports: [
     CommonModule,
     AuthRoutingModule
