e23924fdc718b66642b222c822076cce8713be3a
Fabio <fabioescobarardila@hotmail.com>
2024-03-26 13:22:55 -0500

    Implementing AuthService completed

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
@@ -19,7 +18,6 @@ import { LoginPageComponent } from './pages/login-page/login-page.component';
     CommonModule,
     AuthRoutingModule,
     ReactiveFormsModule,
-    HttpClientModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,27 +1,46 @@
 import { HttpClient } from '@angular/common/http';
-import { inject, Injectable, signal } from '@angular/core';
+import { computed, inject, Injectable, signal } from '@angular/core';
+import { map, Observable, of, tap } from 'rxjs';
 import { environment } from '../../../environments/environments';
-import { Observable, of } from 'rxjs';
+import { AuthStatus, LoginResponse, User } from '../interfaces/index.interface';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class AuthService {
 
-  constructor() { }
-
-  private readonly baseUrl = environment.baseUrl;
+  private readonly myUrl = environment.baseUrl;
   private http = inject(HttpClient);
 
   private _currentUser = signal<User | null>(null);
-  private _authStatus = signal<AuthStatus>();
+  private _authStatus = signal<AuthStatus>(AuthStatus.checking);
+
+  public currentUser = computed( () => this._currentUser())
+  public authStatus = computed( () => this._authStatus())
+
+  constructor() {}
+
+  login( email: string, password: string): Observable<boolean> {
+    const url = `${this.myUrl}/auth/login`;
+    const body = { email, password };
+
+    return this.http.post<LoginResponse>(url, body)
+    .pipe(
+      tap( ({ user, jwt: token }) => {
+        this._currentUser.set( user );
+        this._authStatus.set( AuthStatus.authenticated )
+        localStorage.setItem( 'token', token );
+
+        console.log({user, token});
+      }),
+
+      map(() => true)
+
+      //todo: errores
+    );
 
 
-  login(email: string, password: string): Observable<boolean>{
 
-
-
-
-    return of(true)
   }
+
 }
