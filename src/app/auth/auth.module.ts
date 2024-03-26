6d44552a6b1dfd2fe8277b0ea6c7d36ec7ed90c6
Fabio <fabioescobarardila@hotmail.com>
2024-03-26 09:22:21 -0500

    Implementing AuthService

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,19 +1,25 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { RegisterPageComponent } from './pages/register-page/register-page.component';
 import { AuthLayoutComponent } from './layouts/auth-layout/auth-layout.component';
+import { LoginPageComponent } from './pages/login-page/login-page.component';
 
 
 @NgModule({
   declarations: [
     RegisterPageComponent,
+    LoginPageComponent,
     AuthLayoutComponent
   ],
   imports: [
     CommonModule,
-    AuthRoutingModule
+    AuthRoutingModule,
+    ReactiveFormsModule,
+    HttpClientModule
   ]
 })
 export class AuthModule { }
--- src/app/auth/services/auth.service.ts
+++ src/app/auth/services/auth.service.ts
@@ -1,4 +1,7 @@
-import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environments';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -6,4 +9,19 @@ import { Injectable } from '@angular/core';
 export class AuthService {
 
   constructor() { }
+
+  private readonly baseUrl = environment.baseUrl;
+  private http = inject(HttpClient);
+
+  private _currentUser = signal<User | null>(null);
+  private _authStatus = signal<AuthStatus>();
+
+
+  login(email: string, password: string): Observable<boolean>{
+
+
+
+
+    return of(true)
+  }
 }
